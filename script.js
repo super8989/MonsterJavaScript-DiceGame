d963d050e6cd5553b9ee1a29d6704cf39b408036
Sam <super8989@gmail.com>
2019-08-20 23:02:04 -0400

    2 players roll numbers

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const button = document.querySelector("button");
+const player1 = document.querySelector("player1");
+const player2 = document.querySelector("player2");
+const output = document.querySelector("output");
+
+button.addEventListener("click", function() {
+    let rolls = [roll(6), roll(6)];
+    console.log(rolls);
+})
+
+function roll(num) {
+    let rollNumber = Math.floor(Math.random()*num)+1;
+    return rollNumber;
+}
