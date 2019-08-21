e1155209cfc6293d8fbf11a0070da80ccc2672ab
Sam <super8989@gmail.com>
2019-08-20 23:13:48 -0400

    winner logic conditions

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const button = document.querySelector("button");
-const player1 = document.querySelector("player1");
-const player2 = document.querySelector("player2");
-const output = document.querySelector("output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const output = document.querySelector("#output");
 
 button.addEventListener("click", function() {
     let rolls = [roll(6), roll(6)];
+    let temp;
+    if (rolls[0] == rolls[1]) {
+        temp = "It was a draw";
+    } else if (rolls[0] > rolls[1]) {
+        temp = "Player 1 wins";
+    } else {
+        temp = "Player 2 wins";
+    }
+
+    output.innerHTML = temp;
     console.log(rolls);
+    player1.innerHTML = rolls[0];
+    player2.innerHTML = rolls[1];
 })
 
 function roll(num) {
