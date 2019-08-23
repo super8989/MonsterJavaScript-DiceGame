ab7ac4a77fa66f01f5d988c1fef314c332aa6d66
Sam <super8989@gmail.com>
2019-08-22 22:00:17 -0400

    removing element inside parent

--- script2.js
+++ script2.js
@@ -5,12 +5,18 @@ button.addEventListener("click", function() {
     let rollDice = roll(6); //1-6
     console.log(rollDice);
     let holder = builder(rollDice); //builder(1-6)
+    console.log(output.children[0]);
+
+    if (output.children[0]) {
+        output.children[0].remove();
+    }
+
     output.appendChild(holder);
 })
 
 function builder(num) {
     let div = document.createElement("div");
-    div.setAttribute("class", "dicer");
+    div.setAttribute("class", "dicer"); 
     return div;
 }
 
